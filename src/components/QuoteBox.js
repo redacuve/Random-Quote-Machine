4fcfa55b88966e659abe2c18cbef96c8b2144400
Rey David Cuevas <cuevas.vela@gmail.com>
2020-07-22 11:22:34 -0500

    lint files

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -14,51 +14,58 @@ class QuoteBox extends React.Component {
     this.newQuote = this.newQuote.bind(this);
   }
 
-  newQuote() {
-    this.setState({ index: Math.floor(Math.random() * numberOfQuotes) });
-  }
-
   componentDidMount() {
     fetch(
-      'https://gist.githubusercontent.com/redacuve/0c12440a55d4280ce459d2148e17c971/raw/a27b0f9f0263ba9a35e6a4c59d4601d817b0ee57/quotes.json'
+      'https://gist.githubusercontent.com/redacuve/0c12440a55d4280ce459d2148e17c971/raw/a27b0f9f0263ba9a35e6a4c59d4601d817b0ee57/quotes.json',
     )
-      .then((response) => response.json())
+      .then(response => response.json())
       .then(
-        (result) => {
+        result => {
           this.setState({
             isLoaded: true,
             quotes: result.quotes,
           });
         },
-        (error) => {
+        error => {
           this.setState({
             isLoaded: true,
             error,
           });
-        }
+        },
       );
   }
 
+  newQuote() {
+    this.setState({ index: Math.floor(Math.random() * numberOfQuotes) });
+  }
+
   render() {
-    const { error, isLoaded, quotes, index } = this.state;
+    const {
+      error, isLoaded, quotes, index,
+    } = this.state;
     if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
       return (
-        <div id="quote-box">
-          <p id="text">{quotes[index].quote}</p>
-          <p id="author">{quotes[index].author}</p>
-          <button id="new-quote" onClick={this.newQuote}>
-            New Quote
-          </button>
-          <a href="https://twitter.com/intent/tweet?" id="tweet-quote">
-            Tweet Quote
-          </a>
+        <div>
+          Error:
+          {error.message}
         </div>
       );
     }
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div id="quote-box">
+        <p id="text">{quotes[index].quote}</p>
+        <p id="author">{quotes[index].author}</p>
+        <button id="new-quote" type="button" onClick={this.newQuote}>
+          New Quote
+        </button>
+        <a href="https://twitter.com/intent/tweet?" id="tweet-quote">
+          Tweet Quote
+        </a>
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ ReactDOM.render(
   <React.StrictMode>
     <QuoteBox />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
